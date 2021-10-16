be48a88ec0762e3570efa9cc005071299a62a163
AbrahanV <abrahan.villanuevar@gmail.com>
2021-10-15 22:25:00 -0500

    Mejoras visuales en todas las vistas

--- src/main/java/com/mintic/appcomercio/models/UsuarioModel.java
+++ src/main/java/com/mintic/appcomercio/models/UsuarioModel.java
@@ -11,44 +11,6 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "usuarios")
-<<<<<<< HEAD
-public class UsuarioModel implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long cedula_usuario;
-    private String email_usuario;
-    private String nombre_usuario;
-    private String password;
-    private String usuario;
-	public String getEmail_usuario() {
-		return email_usuario;
-	}
-	public void setEmail_usuario(String email_usuario) {
-		this.email_usuario = email_usuario;
-	}
-	public String getNombre_usuario() {
-		return nombre_usuario;
-	}
-	public void setNombre_usuario(String nombre_usuario) {
-		this.nombre_usuario = nombre_usuario;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUsuario() {
-		return usuario;
-	}
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-}
-=======
 public class UsuarioModel {
     @Id // Define la primera variable declarada como identificador
     @Column(unique = true, nullable = false) // Establece propiedades de la columna en la tabla
@@ -58,4 +20,3 @@ public class UsuarioModel {
     public String password;
     public String usuario;
 }
->>>>>>> ramaVisuales
